c17a0060d07d541ef16e0470b67cf4cbe2652d2d
Babdus <babdusi@gmail.com>
2021-12-14 03:07:33 +0100

    Add jupyter playground

--- main.py
+++ main.py
@@ -1,82 +1,12 @@
-import math
-from typing import Dict, List
-
-import numpy as np
 from scipy.io.wavfile import write
-import random
 
-from note import generate_note
-from schemas import Note, Curve
-
-
-def generate_formants(
-        base_divisor: float = 2,
-        overtone_decay: Curve = Curve.exponential,
-        overtone_random: float = 0.25,
-        n_overtones: int = 16,
-) -> Dict:
-    formants = {}
-    for i in range(n_overtones):
-        if overtone_decay == Curve.constant:
-            formants[i] = 1 / base_divisor
-        elif overtone_decay == Curve.logarithmic:
-            formants[i] = 1 / (base_divisor + math.log(i + 1))
-        elif overtone_decay == Curve.linear:
-            formants[i] = 1 / (base_divisor + i)
-        elif overtone_decay == Curve.quadratic:
-            formants[i] = 1 / (base_divisor * (i + 1))
-        elif overtone_decay == Curve.exponential:
-            formants[i] = 1 / (base_divisor ** (i + 1))
-        else:
-            raise ValueError
-        formants[i] += random.uniform(-formants[i] * overtone_random / 2, formants[i] * overtone_random / 2)
-    print(formants)
-    return formants
-
-
-def generate_harmony(chords: List[np.ndarray], start_times: List[float], sample_rate: int):
-    length = 0
-    for i in range(len(start_times)):
-        start_time = start_times[i]
-        chord = chords[i]
-        end_point = int(start_time * sample_rate) + chord.shape[0]
-        if end_point > length:
-            length = end_point
-
-    harmony = np.zeros(length)
-    for i in range(len(start_times)):
-        start_time = start_times[i]
-        chord = chords[i]
-        step = int(start_time * sample_rate)
-        harmony[step:step + chord.shape[0]] += chord
-    return harmony
-
-
-def generate_notes(note_strings: List[str], **kwargs):
-    cache = {}
-    notes = []
-    for note_string in note_strings:
-        if note_string not in cache:
-            note_list = note_string.split('_')
-            note_value = note_list[0][:-1]
-            octave = int(note_list[0][-1])
-
-            print(note_string, note_list, note_value, octave)
-
-            duration = float(note_list[1])
-
-            cache[note_string] = generate_note(
-                note=getattr(Note, note_value),
-                octave=octave,
-                duration=duration,
-                **kwargs
-            )
-        notes.append(cache[note_string])
-    return notes
+from formant import generate_formants
+from schemas import Curve
+from utils import generate_harmony, generate_notes
 
 
 def shen_khar_venakhi():
-    sample_rate = 4410
+    sample_rate = 44100
 
     chord_formants = generate_formants(
         base_divisor=4,
@@ -231,7 +161,7 @@ def djent():
 
 
 def main():
-    djent()
+    shen_khar_venakhi()
 
 
 if __name__ == "__main__":
